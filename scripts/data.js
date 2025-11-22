79a6672c0743708861e1df05f15384412e23c0ca
NadineJuliana <nadine.juliana.b@web.de>
2025-11-22 18:23:18 +0100

    add amount to data.js

--- scripts/data.js
+++ scripts/data.js
@@ -4,24 +4,28 @@ let mySoups = [
         "price": 6.90,
         "description": "Kokos-Galgant-Suppe",
         "incredients": "Zitronengras, Limettenblätter, Chili, Austernpilze",
+        "amount": 1
     },
     {
         "name": "TOM YAM",
         "price": 6.90,
         "description": "Scharf-saure Suppe",
         "incredients": "Zitronengras, Limettenblätter, Galgant, Thai-Chili Paste, Chili",
+        "amount": 1
     },
     {
         "name": "TOM YAM TALE",
         "price": 9.50,
         "description": "Meeresfrüchte-Suppe",
         "incredients": "Thai-Chilipaste, Austernpilze, Tomaten, Zwiebeln, Galgant, Zitronengras, KaffirLimettenblätter, Thai-Koriander, Chili - scharf",
+        "amount": 1
     },
     {
         "name": "TOM JÜD WUN SEN",
         "price": 6.90,
         "description": "Glasnudelsuppe",
         "incredients": "Kohl, Karotten, Mu-Err-Pilze, Koriander",
+        "amount": 1
     }
 ]
 
@@ -31,18 +35,21 @@ let mySalads = [
         "price": 13.50,
         "description": "Thai-Papapya-Salat",
         "incredients": "Unreife Papaya-Streifen, Knoblauch, Chili, Limette, Erdnüsse",
+        "amount": 1
     },
     {
         "name": "YAM WUN SEN",
         "price": 14.50,
         "description": "Glasnudelsalat mit Garnelen",
         "incredients": "Mu-Err-Pilze, Zwiebeln, Sellerie, Koriander, Tomate, Chili, Erdnüsse",
+        "amount": 1
     },
     {
         "name": "LAAB",
         "price": 13.90,
         "description": "Schweinehack Salat",
         "incredients": "Zitronengras, Minze, Koriander, Limettenblätter, Chili, geröstete Reiskörner",
+        "amount": 1
     }
 ]
 
@@ -52,18 +59,21 @@ let myCurrys = [
         "price": 16.50,
         "description": "Rotes Curry",
         "incredients": "Thai-Basilikum, Bambus, Aubergine, Bohnen, Zucchini, Paprika - leicht scharfe Version",
+        "amount": 1
     },
     {
         "name": "GÄNG KIAO WAAN",
         "price": 16.50,
         "description": "Grünes Curry",
         "incredients": "Thai-Basilikum, Bambus, Aubergine, Bohnen, Zucchini, Paprika - mittel scharfe Version",
+        "amount": 1
     },
     {
         "name": "GÄNG PED PED YANG",
         "price": 16.50,
         "description": "Ananas Curry",
         "incredients": "Thai-Basilikum, Bambus, Aubergine, Bohnen, Tomate, Zucchini, Paprika - leicht scharf",
+        "amount": 1
     }
 ]
 
@@ -73,18 +83,21 @@ let myStreetfoodDishes = [
         "price": 16.90,
         "description": "Schweinehack mit Thai-Basilikum",
         "incredients": "Knoblauch, Chili, Zwiebel, Bohnen",
+        "amount": 1
     },
     {
         "name": "PAD PRIG GÄNG ",
         "price": 18.90,
         "description": "Garnelen mit Currypaste",
         "incredients": "Limettenblätter, Zwiebel, Bambus, Bohnen, Paprika",
+        "amount": 1
     },
     {
         "name": "PAD MED MAMUANG",
         "price": 16.50,
         "description": "Tofu mit Cashewkernen",
         "incredients": "Thai-Chilipaste, Knoblauch, Zwiebel, Paprika, Champignon, Bambu",
+        "amount": 1
     }
 ]
 
@@ -94,24 +107,28 @@ let myNoodleRiceDishes = [
         "price": 15.50,
         "description": "Gebratene Thai-Reisnudeln",
         "incredients": "Reisnudeln mit Ei, Tofu, Lauch, Sprossen, Erdnüsse in hauseigener Sauce auf fruchtiger Tamarinden-Basis gekocht",
+        "amount": 1
     },
     {
         "name": "PAD MIE LÜANG",
         "price": 14.90,
         "description": "Gebratene Nudeln",
         "incredients": "mit Ei, Knoblauch, Kohl, Brokkoli, Karotte, Champignon, Sprossen",
+        "amount": 1
     },
     {
         "name": "KAO PAD",
         "price": 14.90,
         "description": "Gebratener Reis",
         "incredients": "mit Karotte, Ei, Brokkoli, Lauchzwiebel, Tomate ",
+        "amount": 1
     },
     {
         "name": "THAI STYLE SUKIYAKI",
         "price": 16.90,
         "description": "Glasnudeln Gebraten Sukiyaki",
         "incredients": "Sprossen, Chinakohl, Lauch, Sellerie, Sesam, Erdnuss, Limette",
+        "amount": 1
     }
 ]
 
@@ -121,17 +138,22 @@ let myDesserts = [
         "price": 7.90,
         "description": "Kokos-Klebreis mit Mango",
         "incredients": "Mango, Erdnüsse",
+        "amount": 1
     },
     {
         "name": "KAHNOM TOM",
         "price": 6.50,
         "description": "Reismehlbällchen mit Kokosfüllung",
         "incredients": "in Kokosmilch",
+        "amount": 1
     },
     {
         "name": "GLUAI TORD mit Eis",
         "price": 6.90,
         "description": "Gebackene Banane - mit Vanilleeis",
         "incredients": "Sesam, Kokosflocken, Honig",
+        "amount": 1
     }
 ]
+
+let myBasket = []
