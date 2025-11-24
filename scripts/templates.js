8ade22a78931a1001969494e5c9c597fb3376094
NadineJuliana <nadine.juliana.b@web.de>
2025-11-24 17:36:24 +0100

    add tofixed and Subtotal/Sum Templates

--- scripts/templates.js
+++ scripts/templates.js
@@ -4,7 +4,7 @@ function getSoupsTemplate(indexSoup){
     <button onclick="addSoupsToBasket(${indexSoup})">Hinzufügen</button>
     <p>${mySoups[indexSoup].description}</p>
     <p>${mySoups[indexSoup].incredients}</p>
-    <p>${mySoups[indexSoup].price} €</pindexSalad
+    <p>${(mySoups[indexSoup].price).toFixed(2)} €</p>
     `
 }
 
@@ -14,7 +14,7 @@ function getSaladsTemplate(indexSalad){
     <button onclick="addSaladsToBasket(${indexSalad})">Hinzufügen</button>
     <p>${mySalads[indexSalad].description}</p>
     <p>${mySalads[indexSalad].incredients}</p>
-    <p>${mySalads[indexSalad].price} €</p>`
+    <p>${(mySalads[indexSalad].price).toFixed(2)} €</p>`
 }
 
 function getCurrysTemplate(indexCurry){
@@ -23,7 +23,7 @@ function getCurrysTemplate(indexCurry){
     <button onclick="addCurrysToBasket(${indexCurry})">Hinzufügen</button>
     <p>${myCurrys[indexCurry].description}</p>
     <p>${myCurrys[indexCurry].incredients}</p>
-    <p>${myCurrys[indexCurry].price} €</p>
+    <p>${(myCurrys[indexCurry].price).toFixed(2)} €</p>
     `
 }
 
@@ -33,7 +33,7 @@ function getStreetfoodsTemplate(indexStreetfood){
     <button onclick="addStreetfoodToBasket(${indexStreetfood})">Hinzufügen</button>
     <p>${myStreetfoodDishes[indexStreetfood].description}</p>
     <p>${myStreetfoodDishes[indexStreetfood].incredients}</p>
-    <p>${myStreetfoodDishes[indexStreetfood].price} €</p>
+    <p>${(myStreetfoodDishes[indexStreetfood].price).toFixed(2)} €</p>
     `
 }
 
@@ -43,7 +43,7 @@ function getNoddleRiceTemplate(indexNoodleRice){
     <button onclick="addNoodlerRiceToBasket(${indexNoodleRice})">Hinzufügen</button>
     <p>${myNoodleRiceDishes[indexNoodleRice].description}</p>
     <p>${myNoodleRiceDishes[indexNoodleRice].incredients}</p>
-    <p>${myNoodleRiceDishes[indexNoodleRice].price} €</p>
+    <p>${(myNoodleRiceDishes[indexNoodleRice].price).toFixed(2)} €</p>
     `
 }
 
@@ -53,31 +53,9 @@ function getDessertsTemplate(indexDessert){
     <button onclick="addDessertsToBasket(${indexDessert})">Hinzufügen</button>
     <p>${myDesserts[indexDessert].description}</p>
     <p>${myDesserts[indexDessert].incredients}</p>
-    <p>${myDesserts[indexDessert].price} €</p>
+    <p>${(myDesserts[indexDessert].price).toFixed(2)} €</p>
     `
 }
-
-// function getBasketTemplate(){
-//     return`
-    
-//     <div class="amount_content" id="amountRef">
-//         <table>
-//             <tr>
-//                 <th>Zwischensumme</th>
-//                 <td>0 €</td>
-//             </tr>
-//             <tr>
-//                 <th>Lieferkosten</th>
-//                 <td>5 €</td>
-//             </tr>
-//             <tr>
-//                 <th><b>Gesamt</b></th>
-//                 <td><b>0 €</b></td>
-//             </tr>
-//         </table>
-//     </div>
-//     `
-// }
 
 function getBasketRefTemplate(indexBasket){
     return `
@@ -86,8 +64,22 @@ function getBasketRefTemplate(indexBasket){
         <button onclick="calculatePlusBasket(${indexBasket})">+</button>
         <p>${myBasket[indexBasket].amount}</p>
         <button onclick="calculateMinusBasket(${indexBasket})">-</button>
-        <p>${myBasket[indexBasket].price}</p>
+        <p>${(myBasket[indexBasket].price * myBasket[indexBasket].amount).toFixed(2)}</p>
+        <button onclick="deleteFromBasket(${indexBasket})">Löschen</button>
     </div>
     `
 }
 
+function getSubtotalTemplate(subtotal){
+    return `
+    <p>Zwischensumme</p>
+    <p>${subtotal.toFixed(2)} €</p>
+    `
+}
+
+function getSumTemplate(subtotal){
+    return `
+    <p><b>Gesamt</b></p>
+    <p><b>${(subtotal +5).toFixed(2)} €</b></p>
+    `
+}
