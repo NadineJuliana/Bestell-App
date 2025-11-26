05cf442cba87069105f90473f7c950228de32354
NadineJuliana <nadine.juliana.b@web.de>
2025-11-26 17:52:10 +0100

    add div for render function and rearranged buttons

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,59 +1,72 @@
 function getSoupsTemplate(indexSoup){
     return ` 
+    <div class="menu_selection_ref">
     <h4>${mySoups[indexSoup].name}</h4>
-    <button onclick="addSoupsToBasket(${indexSoup})">Hinzufügen</button>
     <p>${mySoups[indexSoup].description}</p>
     <p class="ingredients">${mySoups[indexSoup].ingredients}</p>
     <p>${(mySoups[indexSoup].price).toFixed(2)} €</p>
+    <button onclick="addSoupsToBasket(${indexSoup})">Hinzufügen</button>
+    </div>
     `
 }
 
 function getSaladsTemplate(indexSalad){
     return `
+    <div class="menu_selection_ref">
     <h4>${mySalads[indexSalad].name}</h4>
-    <button onclick="addSaladsToBasket(${indexSalad})">Hinzufügen</button>
     <p>${mySalads[indexSalad].description}</p>
     <p class="ingredients">${mySalads[indexSalad].ingredients}</p>
-    <p>${(mySalads[indexSalad].price).toFixed(2)} €</p>`
+    <p>${(mySalads[indexSalad].price).toFixed(2)} €</p>
+    <button onclick="addSaladsToBasket(${indexSalad})">Hinzufügen</button>
+    </div>
+    `
 }
 
 function getCurrysTemplate(indexCurry){
     return `
+    <div class="menu_selection_ref">
     <h4>${myCurrys[indexCurry].name}</h4>
-    <button onclick="addCurrysToBasket(${indexCurry})">Hinzufügen</button>
     <p>${myCurrys[indexCurry].description}</p>
     <p class="ingredients">${myCurrys[indexCurry].ingredients}</p>
     <p>${(myCurrys[indexCurry].price).toFixed(2)} €</p>
+    <button onclick="addCurrysToBasket(${indexCurry})">Hinzufügen</button>
+    </div>
     `
 }
 
 function getStreetfoodsTemplate(indexStreetfood){
     return `
+    <div class="menu_selection_ref">
     <h4>${myStreetfoodDishes[indexStreetfood].name}</h4>
-    <button onclick="addStreetfoodToBasket(${indexStreetfood})">Hinzufügen</button>
     <p>${myStreetfoodDishes[indexStreetfood].description}</p>
     <p class="ingredients">${myStreetfoodDishes[indexStreetfood].ingredients}</p>
     <p>${(myStreetfoodDishes[indexStreetfood].price).toFixed(2)} €</p>
+    <button onclick="addStreetfoodToBasket(${indexStreetfood})">Hinzufügen</button>
+    </div>
     `
 }
 
 function getNoddleRiceTemplate(indexNoodleRice){
     return `
+    <div class="menu_selection_ref">
     <h4>${myNoodleRiceDishes[indexNoodleRice].name}</h4>
-    <button onclick="addNoodlerRiceToBasket(${indexNoodleRice})">Hinzufügen</button>
     <p>${myNoodleRiceDishes[indexNoodleRice].description}</p>
     <p class="ingredients">${myNoodleRiceDishes[indexNoodleRice].ingredients}</p>
     <p>${(myNoodleRiceDishes[indexNoodleRice].price).toFixed(2)} €</p>
+    <button onclick="addNoodlerRiceToBasket(${indexNoodleRice})">Hinzufügen</button>
+    </div>
     `
 }
 
 function getDessertsTemplate(indexDessert){
     return `
+    <div class="menu_selection_ref">
     <h4>${myDesserts[indexDessert].name}</h4>
     <button onclick="addDessertsToBasket(${indexDessert})">Hinzufügen</button>
     <p>${myDesserts[indexDessert].description}</p>
     <p class="ingredients">${myDesserts[indexDessert].ingredients}</p>
     <p>${(myDesserts[indexDessert].price).toFixed(2)} €</p>
+    </div>
     `
 }
 
@@ -62,9 +75,9 @@ function getBasketRefTemplate(indexBasket){
     <div class="basket_calculator" id="basketContent${indexBasket}">
         <h4>${myBasket[indexBasket].name}</h4>
         <div class="basket_calculate_items">
-        <button onclick="calculatePlusBasket(${indexBasket})">+</button>
-        <p>${myBasket[indexBasket].amount}</p>
         <button onclick="calculateMinusBasket(${indexBasket})">-</button>
+        <p>${myBasket[indexBasket].amount}</p>
+        <button onclick="calculatePlusBasket(${indexBasket})">+</button>
         </div>
         <p>${(myBasket[indexBasket].price * myBasket[indexBasket].amount).toFixed(2)} €</p>
         <button onclick="deleteFromBasket(${indexBasket})">Löschen</button>
